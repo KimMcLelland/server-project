627fd855a0c9792064e0b09b7ba4aa516ad2914d
Kim <faeriekim3@gmail.com>
2021-05-21 10:02:01 +0100

    individual monster from api

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path'); 
+const getMonster = require('./lib/monsters.js');
+// const getIconic = require('./lib/iconic_creatures.js');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); 
+app.engine('hbs', hbs({ 
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views','layouts'), 
+    partialsDir: path.join(__dirname, 'views','partials') 
+})); 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', '.hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async(req, res) => {
+    let data = await getMonster();   
+    res.render('index', { data, listExists: true});
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/iconic', (req, res) => {
+    res.render('iconic');
+});
+
+app.get('*', (req,res)=>{ 
+    res.render('404'); 
+   }); 
 
 app.listen(3000, () => { 
     console.log("Listening to port 3000"); 
